f8b5c590f5fa3171942def8574c4be8df9c8bfea
Felipe Llano <jllanom@eafit.edu.co>
2018-07-30 15:32:09 -0500

    Cambios menores al master

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const Express = require('express');
 const App= Express();
 const connection = require('./server/dbconf/conf')
 
+const message = {"id":1,"title":"Proyectos","name":"proyectos","parent":"0","order":"1","url":"/proyectos","icon":"la la-building","active":"1","interna":"0"}
 App.get('/',(reg,res)=>res.send('puto is gay'));
-App.get('/hola',(reg,res)=>res.send('{"id":1,"title":"Proyectos","name":"proyectos","parent":"0","order":"1","url":"/proyectos","icon":"la la-building","active":"1","interna":"0"}'));
+App.get('/hola',(reg,res)=>res.send(message));
 App.get('/menu',(reg,res)=>{
   // connection.connect(function(err) {
   //   if (err) {
