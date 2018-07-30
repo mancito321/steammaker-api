630ef40e9fbcd94cf13ddf19cdc946c4d8656494
Felipe Llano <jllanom@eafit.edu.co>
2018-07-30 14:49:59 -0500

    Cambios menores al master

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const Express = require('express');
 const App= Express();
+const connection = require('./server/dbconf/conf')
 
-App.get('/',(reg,res)=>res.send('Julian is gay'));
-
-App.listen(process.env.PORT || 5000,()=>console.log('Example app listen on port 5000'))
+App.get('/',(reg,res)=>res.send('puto is gay'));
+App.get('/hola',(reg,res)=>res.send('{"id":1,"title":"Proyectos","name":"proyectos","parent":"0","order":"1","url":"/proyectos","icon":"la la-building","active":"1","interna":"0"}'));
+App.get('/menu',(reg,res)=>{
+  // connection.connect(function(err) {
+  //   if (err) {
+  //     console.error('error connecting: ' + err.stack);
+  //     return;
+  //   }
+  //
+  //   console.log('connected as id ' + connection.threadId);
+  // });
+  connection.query('SELECT * FROM escuela.menu;', function (error, results, fields) {
+    if (error) throw error;
+    res.send(results[0])
+    return results;
+  });
+  //connection.end();
+});
+App.listen(process.env.PORT || 5000,()=>console.log('Example app listen on port 5000'));
---
+++ server/dbconf/conf.js
@@ -0,0 +1,8 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  database : 'escuela'
+});
+
+module.exports = connection;
