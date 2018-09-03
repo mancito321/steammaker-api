1a62a150474f542353924c644526ffe2054ad519
Felipe Llano <jllanom@eafit.edu.co>
2018-09-03 16:47:29 -0500

    credentials for server db

--- server/dbconf/conf.js
+++ server/dbconf/conf.js
@@ -1,8 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
-  user     : 'root',
-  database : 'steammakers'
+  user     : 'user_sm',
+  database : 'steammakers',
+  password : 'sm_Cid2018.'
 });
 
 module.exports = connection;
