c6cf5c8e57ae98fed6439ccf3f969556b059125d
Felipe Llano <jllanom@eafit.edu.co>
2018-09-04 10:46:06 -0500

    Server alone

--- client/src/Components/ChallengeContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const ChallengeContext = createContext({})
-export const ChallengePro = ChallengeContext.Provider
-export const ChallengeCon = ChallengeContext.Consumer
